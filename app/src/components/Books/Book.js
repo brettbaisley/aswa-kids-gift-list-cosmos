e6b4ed508b72dd04ff8a6a72d1fd9c0e6a257011
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2022-12-02 16:22:14 -0500

    Minor styling added

--- app/src/components/Books/Book.js
+++ app/src/components/Books/Book.js
@@ -6,7 +6,7 @@ const Book = (props) => {
             <button className="delete-button">Delete</button>
             <div className="book-element">
                 <p className="title">{props.book.title}</p>
-                <p className="genre">{props.book.genre}</p>
+                <p className="genre">Genre: {props.book.genre}</p>
             </div>
         </li>
     )
