ebfafa0bab5c9e4128f04a760f03af64b6a47c4b
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2022-12-10 19:15:44 -0500

    Updated formatting

--- app/src/components/Gifts/FilterGifts.js
+++ app/src/components/Gifts/FilterGifts.js
@@ -5,13 +5,15 @@ const FilterGifts = () => {
     return (
         <div className="filter-gifts">
             <p>Filter(s):</p>
-            <input type="checkbox" name="hidepurchased" id="hidepurchased" />
-            <label htmlFor="hidepurchased">Show Purchased</label>
+            <div>
+                <input type="checkbox" name="hidepurchased" id="hidepurchased" />
+                <label htmlFor="hidepurchased">Show Purchased</label>
             <select name="children" id="children">
                 <option value="all">All</option>
                 <option value="mateo">Mateo</option>
                 <option value="lucas">Lucas</option>
             </select>
+            </div>
         </div>
     )
 }
