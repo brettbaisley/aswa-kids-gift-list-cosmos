ca652d2c40cf4ca73bb9831bc37f5c76f6b788e3
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2022-12-17 16:24:10 -0500

    auth

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './Header.css';
 
 
+
 const Header = () => {
+    const [userInfo, setUserInfo] = useState();
+    const providers = ['twitter', 'github', 'aad'];
+    const redirect = window.location.pathname;
+
+    useEffect( () => {
+        (async () => {
+            setUserInfo(await getUserInfo() );
+        })();
+    }, []);
+
+    const getUserInfo = async () => {
+        try {
+            const response = await fetch('/.auth/me');
+            const payload = await response.json();
+            const { clientPrincipal } = payload;
+            return clientPrincipal;
+          } catch (error) {
+            console.error('No profile could be found');
+            return undefined;
+          }
+    }
+
     return (
         <header>
             <h1>Gifts</h1>
-            <div className="header-bar"></div> 
+            <div className="menu-list auth">
+                {!userInfo &&
+                providers.map((provider) => (
+                    <a key={provider} href={`/.auth/login/${provider}?post_login_redirect_uri=${redirect}`}>{provider}&nbsp;</a>
+                ))}
+                {userInfo && <a href={`/.auth/logout?post_logout_redirect_uri=${redirect}`}>Logout</a>}
+            </div>
+
+
+            {
+            userInfo && (
+                <div>
+                <div className="user">
+                    <p>Welcome</p>
+                    <p>{userInfo && userInfo.userDetails}</p>
+                    <p>{userInfo && userInfo.identityProvider}</p>
+                </div>
+                </div>
+            )
+            }
+
+
         </header>
     )
 }
