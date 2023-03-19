25df19842cb5511c46700847c2f3228fc79479d8
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2023-03-19 18:54:46 -0400

    Major overhaul

--- app/src/pages/EditGift.js
+++ app/src/pages/EditGift.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import { fetchGiftDB } from '../services/GiftService.mjs';
+import "./EditGift.css";
 
 const EditPage = () => {
     const [gift, setGift] = useState("");
@@ -15,17 +16,25 @@ const EditPage = () => {
 
     return (
         <>
-        <h2>Edit Gift: {id} </h2>
+        <h2>Edit Gift: {gift.title} </h2>
 
         <form>
+            <label htmlFor="gid">ID</label>
+            <input type="text" name="gid" id="gid" value={gift._id} disabled></input>
+
             <label htmlFor="title">Title</label>
             <input type="text" name="title" id="title" value={gift.title}></input>
-
+            
             <label htmlFor="brand">Brand</label>
             <input type="text" name="brand" id="brand" value={gift.brand}></input>
 
             <label htmlFor="price">Price</label>
             <input type="text" name="price" id="price" value={gift.price}></input>
+
+            <div className="form-buttons">
+                <button>Back</button>
+                <button>Submit</button>
+            </div>
 
         </form>
         </>
