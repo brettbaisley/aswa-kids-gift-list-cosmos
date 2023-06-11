6ce1211cfb97cedd3308a04dcac97ea09d3be400
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2023-06-11 19:11:17 -0400

    Fixing build issues

--- app/src/components/GiftItem.js
+++ app/src/components/GiftItem.js
@@ -6,9 +6,12 @@ const DisplayGiftItem = ( {gift, handleStartEdit} ) => {
     const [userInfo] = useAuthContext();
     return (
         <>
-            <p className="title">{gift.title}</p>
-            <p className="brand">{gift.brand}</p>
-            <p className="price">${gift.price}</p>
+            <p className="giftId">{gift._id}</p>
+            <p className="giftTitle">{gift.title}</p>
+            <p className="giftBrand">{gift.brand}</p>
+            <p className="giftPrice">${gift.price}</p>
+            <p className="giftIdPurchased">{gift.purchased ? "Purchased" : "Available"}</p>
+            <p className="giftKids">{gift.kids.join(", ")}</p>
 
             {
                 userInfo && <button onClick={handleStartEdit}><i className="fa-light fa-square-pen"></i></button>
@@ -20,11 +23,18 @@ const DisplayGiftItem = ( {gift, handleStartEdit} ) => {
 const EditGiftItem = ( {gift, handleTextChange, handleUpdateGift, handleStopEdit} ) => {
     return (
         <>
+        <p className="giftId">{gift._id}</p>
+
         <input type="text" name="title" id="title" value={gift.title} onChange={handleTextChange}></input>
         
         <input type="text" name="brand" id="brand" value={gift.brand} onChange={handleTextChange}></input>
 
         <input type="text" name="price" id="price" value={gift.price} onChange={handleTextChange}></input>
+
+        <div><input type="checkbox" name="purchased" id="purchased" checked={gift.purchased}></input><label for="purchased">Purchased</label></div>
+
+        <div><input type="checkbox" name="Mateo" id="Mateo" checked={gift.kids.includes("Mateo")}></input><label for="Mateo">Mateo</label></div>
+        <div><input type="checkbox" name="Lucas" id="Lucas" checked={gift.kids.includes("Lucas")}></input><label for="Lucas">Lucas</label></div>
 
         <div className="btn-group">
             <button className="success" onClick={handleUpdateGift}><i className="fa-light fa-pen"></i></button>
